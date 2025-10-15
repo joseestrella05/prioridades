2e0c11d85f66372a72b3e86f6e69e06a1f37c08d
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-10-15 15:57:28 -0400

    add despendencia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ dependencies {
     implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.11.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
