a5a3e5d275c073d16ae230cc8cbe693b1eb386e9
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-10-15 16:02:05 -0400

    add RetrofitClient.kt

---
+++ app/src/main/java/edu/ucne/prioridades/data/network/RetrofitClient.kt
@@ -0,0 +1,28 @@
+package edu.ucne.prioridades.data.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitClient {
+    private const val BASE_URL = "https://prioridad-api.onrender.com/api/"
+
+    private val client = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }).build()
+
+    val api: ApiService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(ApiService::class.java)
+    }
+}
